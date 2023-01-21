e379b92444a1c1e45f16b9318324f734a7385546
Cooper Gillan <cooper.gillan@gmail.com>
2023-01-21 19:50:47 +0000

    Passing tests for global functions accepting Rectangle

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -1,13 +1,18 @@
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
-fn perimeter(width: f64, height: f64) -> f64 {
-    2.0 * (width + height)
+fn perimeter(rectangle: Rectangle) -> f64 {
+    2.0 * (rectangle.width + rectangle.height)
 }
 
-fn area(width: f64, height: f64) -> f64 {
-    width * height
+fn area(rectangle: Rectangle) -> f64 {
+    rectangle.width * rectangle.height
 }
 
 #[cfg(test)]
@@ -16,14 +21,16 @@ mod tests {
 
     #[test]
     fn test_perimeter() {
-        let got = perimeter(10.0, 10.0);
+        let rectangle = Rectangle { width: 10.0, height: 10.0 };
+        let got = perimeter(rectangle);
         let want = 40.0;
         assert_eq!(got, want, "got {:.2}, wanted {:.2}", got, want);
     }
 
     #[test]
     fn test_area() {
-        let got = area(12.0, 6.0);
+        let rectangle = Rectangle { width: 12.0, height: 6.0 };
+        let got = area(rectangle);
         let want = 72.0;
         assert_eq!(got, want, "got {:.2}, wanted {:.2}", got, want);
     }
