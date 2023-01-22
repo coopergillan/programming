35fff4e5ec40041785f7ab905a17fbeeed72b21a
Cooper Gillan <cooper.gillan@gmail.com>
2023-01-22 05:14:33 +0000

    Clean up the enum solution

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -1,12 +1,3 @@
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-struct Circle {
-    radius: f64,
-}
-
 enum Shape {
     Rectangle { width: f64, height: f64 },
     Circle { radius: f64 },
@@ -17,14 +8,12 @@ impl Shape {
         match *self {
             Shape::Rectangle { width, height } => (width * 2.0) + (height * 2.0),
             Shape::Circle { radius } => 2.0 * 3.14159265358979 * radius,
-            _ => 0.0,
         }
     }
     fn area(&self) -> f64 {
         match self {
             Shape::Rectangle { width, height } => width * height,
             Shape::Circle { radius } => 3.14159265358979 * (radius * radius),
-            _ => 0.0,
         }
     }
 }
@@ -32,18 +21,6 @@ impl Shape {
 fn main() {
     println!("Hello, world!");
 }
-
-// fn perimeter(rectangle: Rectangle) -> f64 {
-//     2.0 * (rectangle.width + rectangle.height)
-// }
-
-// fn area(rectangle: Rectangle) -> f64 {
-//     rectangle.width * rectangle.height
-// }
-//
-// fn area(circle: Circle) -> f64 {
-//     3.14159265358979 * (circle.radius * circle.radius)
-// }
 
 #[cfg(test)]
 mod tests {
@@ -57,23 +34,25 @@ mod tests {
         };
         let got = rectangle.perimeter();
         let want = 40.0;
-        assert_eq!(got, want, "got {:.2}, wanted {:.2}", got, want);
+        assert_eq!(got, want, "got {:?}, wanted {:?}", got, want);
     }
 
     #[test]
     fn test_rectangle_area() {
-        let rectangle = Shape::Rectangle { width: 12.0, height: 6.0 };
+        let rectangle = Shape::Rectangle {
+            width: 12.0,
+            height: 6.0,
+        };
         let got = rectangle.area();
         let want = 72.0;
         assert_eq!(got, want, "got {:.2}, wanted {:.2}", got, want);
     }
 
     #[test]
-    #[ignore]
     fn test_circle_area() {
         let circle = Shape::Circle { radius: 6.0 };
         let got = circle.area();
-        let want = 314.1592653589793;
+        let want = 113.09733552923244;
         assert_eq!(got, want, "got {:.20}, wanted {:.20}", got, want);
     }
 }
