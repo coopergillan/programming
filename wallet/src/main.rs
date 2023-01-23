4001d2d7d0bc76ffe53ce28abf1114dee4365cd3
Cooper Gillan <cooper.gillan@gmail.com>
2023-01-23 04:57:56 +0000

    Add withdraw method, refactor tests some
    Implement `SubAssign` for the first time, then refactor some of the
    tests, setting up the question of an overdraft next.

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,5 +1,5 @@
 use std::fmt;
-use std::ops::AddAssign;
+use std::ops::{AddAssign, SubAssign};
 
 #[derive(Debug, PartialEq)]
 struct Bitcoin(isize);
@@ -10,6 +10,13 @@ impl AddAssign for Bitcoin {
         self.0 += other.0
     }
 }
+
+impl SubAssign for Bitcoin {
+    fn sub_assign(&mut self, other: Self) {
+        self.0 -= other.0
+    }
+}
+
 impl fmt::Display for Bitcoin {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{} BTC", self.0)
@@ -21,14 +28,18 @@ struct Wallet {
 }
 
 impl Wallet {
-    fn new() -> Self {
+    fn new(starting_balance: Bitcoin) -> Self {
         Wallet {
-            balance: Bitcoin(0),
+            balance: starting_balance,
         }
     }
 
     fn deposit(&mut self, amount: Bitcoin) {
         self.balance += amount;
+    }
+
+    fn withdraw(&mut self, amount: Bitcoin) {
+        self.balance -= amount;
     }
 }
 
@@ -40,14 +51,26 @@ fn main() {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_wallet() {
-        let mut wallet = Wallet::new();
+    fn wallet(starting_balance: isize) -> Wallet {
+        Wallet::new(Bitcoin(starting_balance))
+    }
 
-        wallet.deposit(Bitcoin(10));
+    fn assert_balance(wallet: Wallet, want: Bitcoin) {
         let got = wallet.balance;
-        let want = Bitcoin(10);
-
         assert_eq!(got, want, "got {} want {}", got, want);
+    }
+
+    #[test]
+    fn test_wallet_deposit() {
+        let mut wallet = wallet(0);
+        wallet.deposit(Bitcoin(10));
+        assert_balance(wallet, Bitcoin(10));
+    }
+
+    #[test]
+    fn test_wallet_withdrawal() {
+        let mut wallet = wallet(20);
+        wallet.withdraw(Bitcoin(10));
+        assert_balance(wallet, Bitcoin(10));
     }
 }
