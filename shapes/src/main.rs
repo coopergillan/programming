32bea1058206aa804e60598043ea03977016ba02
Cooper Gillan <cooper.gillan@gmail.com>
2023-01-23 04:14:01 +0000

    Add triangle, plus test generation macros
    Bring this stage to a close by:
    
    * Importing the PI contanst
    * Adding Triangle to the shapes
    * Copying some macros syntax to define multiple tests

--- shapes/src/main.rs
+++ shapes/src/main.rs
@@ -1,58 +1,85 @@
+use std::f64::consts::PI;
+
 enum Shape {
     Rectangle { width: f64, height: f64 },
     Circle { radius: f64 },
+    Triangle { base: f64, height: f64 },
 }
 
 impl Shape {
     fn perimeter(&self) -> f64 {
-        match *self {
+        match self {
             Shape::Rectangle { width, height } => (width * 2.0) + (height * 2.0),
-            Shape::Circle { radius } => 2.0 * 3.14159265358979 * radius,
+            Shape::Circle { radius } => 2.0 * PI * radius,
+            Shape::Triangle { base: _, height: _ } => {
+                println!("Unable to calculate perimeter of triangle from base and height!");
+                -1.0
+            }
         }
     }
     fn area(&self) -> f64 {
         match self {
             Shape::Rectangle { width, height } => width * height,
-            Shape::Circle { radius } => 3.14159265358979 * (radius * radius),
+            Shape::Circle { radius } => PI * (radius * radius),
+            Shape::Triangle { base, height } => 0.5 * base * height,
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! All the action is in the tests.");
+}
+
+// Taken from the following StackOverflow answer:
+// https://stackoverflow.com/questions/34662713/how-can-i-create-parameterized-tests-in-rust
+// A little surprised that this is the only way to actually create separate tests
+macro_rules! area_tests {
+    ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                check_area(input, expected);
+            }
+        )*
+    }
+}
+
+macro_rules! perimeter_tests {
+    ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                check_perimeter(input, expected);
+            }
+        )*
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_rectangle_perimeter() {
-        let rectangle = Shape::Rectangle {
-            width: 10.0,
-            height: 10.0,
-        };
-        let got = rectangle.perimeter();
-        let want = 40.0;
-        assert_eq!(got, want, "got {:?}, wanted {:?}", got, want);
+    fn check_area(shape: Shape, want: f64) {
+        let got = shape.area();
+        assert_eq!(got, want, "got {:.2}, but want {:.2}", got, want);
     }
 
-    #[test]
-    fn test_rectangle_area() {
-        let rectangle = Shape::Rectangle {
-            width: 12.0,
-            height: 6.0,
-        };
-        let got = rectangle.area();
-        let want = 72.0;
-        assert_eq!(got, want, "got {:.2}, wanted {:.2}", got, want);
+    fn check_perimeter(shape: Shape, want: f64) {
+        let got = shape.perimeter();
+        assert_eq!(got, want, "got {:.2}, but want {:.2}", got, want);
     }
 
-    #[test]
-    fn test_circle_area() {
-        let circle = Shape::Circle { radius: 6.0 };
-        let got = circle.area();
-        let want = 113.09733552923244;
-        assert_eq!(got, want, "got {:.20}, wanted {:.20}", got, want);
+    area_tests! {
+        rectangle_area: (Shape::Rectangle { width: 12.0, height: 6.0, }, 72.0),
+        circle_area: (Shape::Circle { radius: 6.0 }, 113.09733552923255),
+        triangle_area: (Shape::Triangle { base: 6.0, height: 12.0 }, 36.0),
+    }
+
+    perimeter_tests! {
+        rectangle_perimeter: (Shape::Rectangle { width: 12.0, height: 6.0, }, 36.0),
+        circle_perimeter: (Shape::Circle { radius: 6.0 }, 37.69911184307752),
+        triangle_perimeter: (Shape::Triangle { base: 6.0, height: 12.0 }, -1.0),
     }
 }
