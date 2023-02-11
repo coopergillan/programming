0ac9bd4c717acd97a17d91325c20871f2631e2c1
Cooper Gillan <cooper.gillan@gmail.com>
2023-02-11 03:56:12 +0000

    Read fake fixed width record and prepare for writing CSV

--- csv_examples/src/main.rs
+++ csv_examples/src/main.rs
@@ -1,19 +1,67 @@
-use chrono::prelude::*;
+use chrono::NaiveDate;
+use fixed_width_derive::FixedWidth;
+// use serde::Serialize;
+use serde_derive::Deserialize;
+use std::error::Error;
+// use std::io;
+use std::process;
 
-#[derive(Debug)]
+#[allow(dead_code)]
+#[allow(unused_attributes)]
+#[derive(Debug, FixedWidth, Deserialize)]
 struct SmallStreamedRecord {
+    #[fixed_width(range = "0..20")]
     pub show_name: String,
+    #[fixed_width(range = "20..30")]
     pub view_date: NaiveDate,
+    #[fixed_width(range = "30..40")]
     pub category: String,
+    #[fixed_width(range = "40..44")]
     pub charge_amount: isize,
 }
 
+// #[derive(Debug, Deserialize)]
+// struct DateRecord {
+//     pub view_date: Option<NaiveDate>,
+// }
+//
+// impl OrigFixedWidth for DateRecord {
+//     fn fields() -> FieldSet {
+//         FieldSet::Seq(vec![FieldSet::new_field(20..30)])
+//     }
+// }
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let raw_data = "        MOTOWN MAGIC2023-02-05      KIDS1489";
+    let raw_width = raw_data.len();
+
+    let mut reader = fixed_width::Reader::from_string(raw_data).width(raw_width);
+
+    for record in reader.byte_reader().filter_map(std::result::Result::ok) {
+        let streamed_record: SmallStreamedRecord = fixed_width::from_bytes(&record)?;
+        println!("streamed_record: {:?}", streamed_record);
+    }
+
+    // let record = SmallStreamedRecord {
+    //     show_name: "MOTOWN MAGIC".to_string(),
+    //     view_date: NaiveDate::from_ymd_opt(2023, 2, 5).unwrap(),
+    //     category: "KIDS".to_string(),
+    //     charge_amount: 1489,
+    // };
+    // println!("record: {:?}", record);
+    //
+    // let mut wtr = csv::WriterBuilder::new()
+    //     // .quote_style(csv::QuoteStyle::NonNumeric)
+    //     .from_writer(io::stdout());
+    //
+    // wtr.serialize(record)?;
+    // wtr.flush()?;
+    Ok(())
+}
+
 fn main() {
-    let record = SmallStreamedRecord {
-        show_name: "Motown Magic".to_string(),
-        view_date: NaiveDate::from_ymd_opt(2023, 2, 5).unwrap(),
-        category: "Kids".to_string(),
-        charge_amount: 1489,
-    };
-    println!("record: {:?}", record);
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
