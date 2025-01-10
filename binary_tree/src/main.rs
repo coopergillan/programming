dc2839c39cd694678ddc1b2bdda9771761acbaea
Cooper Gillan <cooper.gillan@gmail.com>
2025-01-10 16:01:49 +0000

    Add binary tree progress to this point

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -5,6 +5,27 @@ enum BinaryTree<T> {
     NonEmpty(Box<TreeNode<T>>),
 }
 
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty => {
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty,
+                }))
+            }
+            BinaryTree::NonEmpty(ref mut node) => {
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+            }
+        }
+    }
+}
+
 // A part of a BinaryTree
 #[derive(Debug)]
 struct TreeNode<T> {
@@ -16,46 +37,60 @@ struct TreeNode<T> {
 use self::BinaryTree::*;
 
 fn main() {
-    let jupiter_tree = NonEmpty(Box::new(TreeNode {
-        element: "Jupiter",
-        left: Empty,
-        right: Empty,
-    }));
-
-    let mercury_tree = NonEmpty(Box::new(TreeNode {
-        element: "Mercury",
-        left: Empty,
-        right: Empty,
-    }));
-
-    let mars_tree = NonEmpty(Box::new(TreeNode {
-        element: "Mars",
-        left: jupiter_tree,
-        right: mercury_tree,
-    }));
-
-    let venus_tree = NonEmpty(Box::new(TreeNode {
-        element: "Venus",
-        left: Empty,
-        right: Empty,
-    }));
-
-    let uranus_tree = NonEmpty(Box::new(TreeNode {
-        element: "Uranus",
-        left: Empty,
-        right: venus_tree,
-    }));
-
-    let tree = NonEmpty(Box::new(TreeNode {
-        element: "Saturn",
-        left: mars_tree,
-        right: uranus_tree,
-    }));
+    // let jupiter_tree = NonEmpty(Box::new(TreeNode {
+    //     element: "Jupiter",
+    //     left: Empty,
+    //     right: Empty,
+    // }));
+    //
+    // let mercury_tree = NonEmpty(Box::new(TreeNode {
+    //     element: "Mercury",
+    //     left: Empty,
+    //     right: Empty,
+    // }));
+    //
+    // let mars_tree = NonEmpty(Box::new(TreeNode {
+    //     element: "Mars",
+    //     left: jupiter_tree,
+    //     right: mercury_tree,
+    // }));
+    //
+    // let venus_tree = NonEmpty(Box::new(TreeNode {
+    //     element: "Venus",
+    //     left: Empty,
+    //     right: Empty,
+    // }));
+    //
+    // let uranus_tree = NonEmpty(Box::new(TreeNode {
+    //     element: "Uranus",
+    //     left: Empty,
+    //     right: venus_tree,
+    // }));
+    //
+    // let tree = NonEmpty(Box::new(TreeNode {
+    //     element: "Saturn",
+    //     left: mars_tree,
+    //     right: uranus_tree,
+    // }));
 
     // let tree = NonEmpty(Box::new(TreeNode {
     //     element: "Saturn",
     //     left: mars_tree,
-
-    println!("Hello, world!\n");
+    let mut tree = BinaryTree::Empty;
     println!("tree: {:?}", &tree);
+
+    tree.add("Mercury");
+    tree.add("Sun");
+    tree.add("Venus");
+
+    println!("tree: {:?}", &tree);
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_it() {
+        assert!(false);
+    }
 }
