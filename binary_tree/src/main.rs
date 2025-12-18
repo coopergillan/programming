81ade0ec65df1804936ceece584d6bf01e152a62
Cooper Gillan <cooper.gillan@gmail.com>
2025-12-18 17:15:35 +0000

    Have a little fun with integers with binary trees and heaps

--- binary_tree/src/main.rs
+++ binary_tree/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 // An ordered collection of `T`s
 #[derive(Debug)]
 enum BinaryTree<T> {
@@ -16,8 +18,10 @@ impl<T: Ord> BinaryTree<T> {
                 }))
             }
             BinaryTree::NonEmpty(ref mut node) => {
+                // The left tree is for the smaller or equal value
                 if value <= node.element {
                     node.left.add(value);
+                // The right tree is for the greater value
                 } else {
                     node.right.add(value);
                 }
@@ -37,55 +41,41 @@ struct TreeNode<T> {
 use self::BinaryTree::*;
 
 fn main() {
-    // let jupiter_tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Jupiter",
-    //     left: Empty,
-    //     right: Empty,
-    // }));
-    //
-    // let mercury_tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Mercury",
-    //     left: Empty,
-    //     right: Empty,
-    // }));
-    //
-    // let mars_tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Mars",
-    //     left: jupiter_tree,
-    //     right: mercury_tree,
-    // }));
-    //
-    // let venus_tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Venus",
-    //     left: Empty,
-    //     right: Empty,
-    // }));
-    //
-    // let uranus_tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Uranus",
-    //     left: Empty,
-    //     right: venus_tree,
-    // }));
-    //
-    // let tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Saturn",
-    //     left: mars_tree,
-    //     right: uranus_tree,
-    // }));
+    // Adding values with a binary tree
+    let mut tree = NonEmpty(Box::new(TreeNode {
+        element: 57,
+        left: Empty,
+        right: Empty,
+    }));
+    dbg!(&tree);
 
-    // let tree = NonEmpty(Box::new(TreeNode {
-    //     element: "Saturn",
-    //     left: mars_tree,
-    let mut tree = BinaryTree::Empty;
-    println!("tree: {:?}", &tree);
+    tree.add(58);
+    tree.add(60);
+    dbg!(&tree);
 
-    tree.add("Mercury");
-    tree.add("Sun");
-    tree.add("Venus");
+    tree.add(59);
+    dbg!(&tree);
 
-    println!("tree: {:?}", &tree);
+    tree.add(17);
+    tree.add(9);
+    dbg!(&tree);
+
+    // Now do the same numbers in a BinaryHeap
+    let mut heap = BinaryHeap::new();
+    heap.push(57);
+    heap.push(58);
+    heap.push(59);
+    heap.push(9);
+    heap.push(17);
+    dbg!(&heap);
+
+    while heap.len() > 0 {
+        let value = heap.pop();
+        dbg!(value);
+    }
 }
 
+#[cfg(test)]
 mod tests {
     use super::*;
 
